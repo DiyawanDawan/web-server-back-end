e600f3a5b3d4d8217fdee417f166fbdaeb7ad707
DiyawanDawan <diyawan0@gmail.com>
2023-02-22 16:26:40 +0800

    fix Semoga bisa

--- src/handler.js
+++ src/handler.js
@@ -18,7 +18,7 @@ const addNoteHandler = (request, h) => {
 
     if (isSuccess) {
         const response = h.response({
-            error: false, message: 'Catatan berhasil ditambahkan',
+            status: "Sukses", message: 'Catatan berhasil ditambahkan',
 
             data: {
                 noteId: id,
--- src/server.js
+++ src/server.js
@@ -6,12 +6,12 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
-        cors: {
-          origin: ['*'],
-        },
+      cors: {
+        origin: ['*'],
       },
+    },
   });
  
   server.route(routes);
